6e8b07b7c5d9d2046e244f5b3716a90b3928644d
SimonRichardson <stickupkid@gmail.com>
2015-05-14 18:08:47 +0100

    Starting to implement commands
     - Allowing connection to Redis - including:
       - connecting
       - disconnecting
     - Started implementing a simple query language, to prevent the anything goes attitude.
     - Adding example

--- examples/Examples/Database/Redis/Get.purs
+++ examples/Examples/Database/Redis/Get.purs
@@ -1,17 +1,18 @@
-module Examples.Database.Redis.Find where
+module Examples.Database.Redis.Get where
 
 import Database.Redis.Redis
-import Database.Redis.ConnectionInfo
-import Database.Redis.Bson.BsonValue
+import Database.Redis.Commands
 
 import Control.Monad.Aff
 import Control.Monad.Eff
 import Control.Monad.Eff.Class
 import Control.Monad.Eff.Exception
 
+import Data.Array
 import Data.Either
 import Data.Event
 import Data.Maybe
+import Data.Tuple
 
 import Debug.Trace
 
@@ -25,5 +26,23 @@ foreign import traceAny
   }
   """ :: forall e a. a -> Eff (trace :: Trace | e) Unit
 
+uri :: String
+uri = "redis://127.0.0.1:6379"
+
 main = launchAff $ do
-  liftEff $ uint
+  Right database <- attempt $ connect uri
+
+  query (MSet [Tuple "key" "value", Tuple "key" "value"]) database
+  res <- query (Sort "list" 
+    [ BY $ FIELD "hash:*" "field"
+    , LIMIT 2 3
+    , GET $ NAME "gk"
+    , GET $ NAME "#"
+    , GET $ FIELD "gh" "f*"
+    , DESC
+    , ALPHA
+    , STORE $ NAME "store"
+    ]) database
+  liftEff $ traceAny res
+
+  close database
