ab67c3b25d309619bd1cf81027022552e08836d7
SimonRichardson <stickupkid@gmail.com>
2015-06-29 22:20:09 +0100

    Tweaking the field command

--- src/Database/Redis/Commands/Field.purs
+++ src/Database/Redis/Commands/Field.purs
@@ -9,6 +9,10 @@ instance valField :: Val (Field a) where
   value (Field v) = v
 
 data Name
+data Prop
 
 name :: String -> Field Name
 name n = Field $ fromString n
+
+field :: String -> String -> Field Prop
+field a b = Field $ (fromString a <> fromString "->" <> fromString b)
